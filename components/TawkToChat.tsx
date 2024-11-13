02ee8a2113a96130eec880b9d0416c91c2c2ef77
BMNKUNA <bmwnkuna@gmail.com>
2024-11-13 13:34:26 +0200

    updatingafter repairing app

--- components/TawkToChat.tsx
+++ components/TawkToChat.tsx
@@ -14,7 +14,10 @@ export default function TawkToChat() {
       s1.src = 'https://embed.tawk.to/673486682480f5b4f59d7bf3/1iciiq2nf';
       s1.charset = 'UTF-8';
       s1.setAttribute('crossorigin', '*');
-      s0.parentNode.insertBefore(s1, s0);
+      
+      if (s0 && s0.parentNode) {
+        s0.parentNode.insertBefore(s1, s0);
+      }
     })();
   }, []);
 
